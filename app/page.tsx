53148ce20af74404c0051cc0527dbbf73d08f8cc
witttccchhher <somokill650@gmail.com>
2025-05-14 18:31:26 +0500

    Fix building error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
