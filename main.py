7d2381d6d432aabe9d6fbacc0be6235e2db1d11c
witttccchhher <somokill650@gmail.com>
2025-05-14 18:37:07 +0500

    Fix allowed urls

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://nextpass.vercel.app"
+        "https://nextpass-zeta.vercel.app"
     ],  # TODO: указать домен фронта
     allow_credentials=True,
     allow_methods=["*"],
